60222cb13f031501b230a15204813bc091372647
KevinCastellanosAmador <kearcastellanosam@ittepic.edu.mx>
2025-05-21 19:18:15 -0700

    Creación de la estructura basica del proyecto

---
+++ Config/App/Views.php
@@ -0,0 +1,15 @@
+<?php
+class Views{
+
+    public function getView($pagina, $vista, $data="")
+    {
+        if ($pagina == "principal") {
+            $vista = "Views/".$vista.".php";
+        }else{
+            $vista = "Views/".$pagina."/".$vista.".php";
+        }
+        require $vista;
+    }
+}
+
+?>
---
+++ Config/Config.php
@@ -0,0 +1,8 @@
+<?php
+const BASE_URL = "http://localhost/InnovaTube/";
+const HOST = "localhost";
+const USER = "root";
+const PASS = "";
+const DB = "";
+const CHARSET = "charset=UTF8";
+?>
